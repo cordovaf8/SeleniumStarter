f966475f800900cc54f0221aa48dbe7fef887d38
Forrest Cordova <fcordova0616@gmail.com>
2023-08-15 20:39:33 -0500

    teset

--- SeleniumStarter/Pages/Pages.cs
+++ SeleniumStarter/Pages/Pages.cs
@@ -16,14 +16,6 @@ namespace SeleniumStarter.Pages
         public IWebDriver Driver { get; set; }
 
         public HomePage HomePage { get; set; }
-        public FormsPage FormsPage { get; set; }
-        public PracticeFormPage PracticeFormPage { get; set; }
-        public LinksPage LinksPage { get; set; }
-        public LoginPage LoginPage { get; set; }
-        public BookStorePage BookStorePage { get; set; }
-        public ProfilePage ProfilePage { get; set; }
-        public WebTablePage WebTablePage { get; set; }
-        public IFramePage IFramePage { get; set; }
         public Shared Shared { get; set; }
 
         public Pages(IWebDriver driver, Interaction interaction)
@@ -33,15 +25,9 @@ namespace SeleniumStarter.Pages
 
             #region DemoQA Pages
             HomePage = new(driver, interaction);
-            FormsPage = new(driver, interaction);
-            PracticeFormPage = new(driver, interaction);
-            LinksPage = new(driver, interaction);
-            LoginPage = new(driver, interaction);
-            BookStorePage = new(driver, interaction);
-            ProfilePage = new(driver, interaction);
+
             Shared = new(driver, interaction);
-            WebTablePage = new(driver, interaction);
-            IFramePage = new(driver, interaction);
+ 
 
             #endregion
         }
